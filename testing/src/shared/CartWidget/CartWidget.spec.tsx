724212b67065ab86a48e6b9ff14c77627aa24351
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-07-03 18:52:31 +0200

    Test CartWidget

--- testing/src/shared/CartWidget/CartWidget.spec.tsx
+++ testing/src/shared/CartWidget/CartWidget.spec.tsx
@@ -1,33 +1,26 @@
-import React from "react"
-import { CartWidget } from "./CartWidget"
-import { fireEvent } from "@testing-library/react"
+import React from 'react';
+import { CartWidget } from './CartWidget';
+import { fireEvent } from '@testing-library/react';
 
-describe("CartWidget", () => {
-  it("shows the amount of products in the cart", () => {
+describe('CartWidget', () => {
+  it('navigates to cart summary page on click', () => {
+    const { getByRole, history } = global.renderWithRouter(() => <CartWidget />);
+    fireEvent.click(getByRole('link'));
+    expect(history.location.pathname).toEqual('/cart');
+  });
+
+  it('shows the amount of products in the cart', () => {
     const stubCartHook = () => ({
       products: [
         {
-          name: "Product foo",
+          name: 'Product foo',
           price: 0,
-          image: "image.png"
-        }
+          image: 'image.png',
+        },
       ],
-    })
+    });
 
-    const { container } = renderWithRouter(() => (
-      <CartWidget useCartHook={stubCartHook} />
-    ))
-
-    expect(container.innerHTML).toMatch("1")
-  })
-
-  it("navigates to cart summary page on click", () => {
-    const { getByRole, history } = renderWithRouter(() => (
-      <CartWidget />
-    ))
-
-    fireEvent.click(getByRole("link"))
-    
-    expect(history.location.pathname).toEqual("/cart")
-  })
-})
+    const { container } = global.renderWithRouter(() => <CartWidget useCartHook={stubCartHook} />);
+    expect(container.innerHTML).toMatch('1');
+  });
+});
