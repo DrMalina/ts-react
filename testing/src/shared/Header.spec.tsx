ed89a6cf865ce0cea8a59addcf8d14d528652d9f
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-07-03 18:52:03 +0200

    Test Header  Component

--- testing/src/shared/Header.spec.tsx
+++ testing/src/shared/Header.spec.tsx
@@ -1,21 +1,21 @@
-import React from "react"
-import { Header } from "./Header"
-import { fireEvent } from "@testing-library/react"
+import React from 'react';
+import { Header } from './Header';
+import { fireEvent } from '@testing-library/react';
 
-jest.mock("./CartWidget", () => ({
-  CartWidget: () => <div>Cart widget</div>
-}))
+jest.mock('./CartWidget', () => ({
+  CartWidget: () => <div>Cart widget</div>,
+}));
 
-describe("Header", () => {
-  it("renders correctly", () => {
-    const { container } = renderWithRouter(() => <Header />)
-    expect(container.innerHTML).toMatch("Goblin Store")
-    expect(container.innerHTML).toMatch("Cart widget")
-  })
+describe('Header', () => {
+  it('renders correctly', () => {
+    const { container } = global.renderWithRouter(() => <Header />);
+    expect(container.innerHTML).toMatch('Goblin Store');
+    expect(container.innerHTML).toMatch('Cart widget');
+  });
 
-  it("navigates to / on header title click", () => {
-    const { getByText, history } = renderWithRouter(() => <Header />)
-    fireEvent.click(getByText("Goblin Store"))
-    expect(history.location.pathname).toEqual("/")
-  })
-})
+  it('navigates to / on header title click', () => {
+    const { getByText, history } = global.renderWithRouter(() => <Header />);
+    fireEvent.click(getByText('Goblin Store'));
+    expect(history.location.pathname).toEqual('/');
+  });
+});
