a2b87e9d362540941ec45c528db313db50349b72
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-07-25 00:23:17 +0200

    Render page for each post

---
+++ news/pages/post/[id].tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { useRouter } from 'next/router';
+
+const Post: FC = () => {
+  const { pathname, query } = useRouter();
+
+  return (
+    <div>
+      Pathname: {pathname};<br />
+      Post Id: {query.id}.
+    </div>
+  );
+};
+
+export default Post;
