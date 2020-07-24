35c4614083e9f71556d8d731871cd21e750899c7
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-07-25 00:18:03 +0200

    Add Feed

---
+++ news/components/Feed/Feed.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { Section } from '../Section';
+
+export const Feed: FC = () => {
+  return (
+    <>
+      <Section title="Science" />
+      <Section title="Technology" />
+      <Section title="Arts" />
+    </>
+  );
+};
