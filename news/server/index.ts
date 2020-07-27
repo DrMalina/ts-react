209b8530a4de5bc58c60b2782d4e7fd0933c53ae
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-07-27 11:33:06 +0200

    Add /posts and /categories endpoints

--- news/server/index.ts
+++ news/server/index.ts
@@ -8,3 +8,15 @@ const posts = require('./posts.json');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+const port = 4000;
+
+app.get('/posts', (_, res) => {
+  return res.json(posts);
+});
+
+app.get('/categories', (_, res) => {
+  return res.json(categories);
+});
+
+app.listen(port, () => console.log(`DB is running on http://localhost:${port}!`));
