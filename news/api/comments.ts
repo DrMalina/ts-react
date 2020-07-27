c1e7fa2ce4fc7ab47cfd2ed7eea157f2fe3fa657
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-07-27 23:25:14 +0200

    Create fn for submitting comments

--- news/api/comments.ts
+++ news/api/comments.ts
@@ -6,3 +6,15 @@ export async function fetchComments(postId: EntityId): Promise<Comment[]> {
   const res = await fetch(`${config.baseUrl}/comments/${postId}`);
   return await res.json();
 }
+
+export async function submitComment(
+  postId: EntityId,
+  name: Person,
+  comment: string,
+): Promise<Response> {
+  return await fetch(`${config.baseUrl}/posts/${postId}/comments`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    body: JSON.stringify({ name, comment }),
+  });
+}
